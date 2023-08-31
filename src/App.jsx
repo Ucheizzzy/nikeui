c96ea72348df11fccdeaf7449a901a84c1bdf585
Ucheizzzy <uchechukwuobiakor@gmail.com>
2023-08-31 14:32:11 +0100

    Creating the sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 const App = () => {
-  return <div>App</div>
+  return (
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-1 wide:adding-r padding-b'>Hero</section>
+      <section className='padding'>popular products</section>
+      <section className='padding'>super quality</section>
+      <section className='padding-x py-10'>Services</section>
+      <section className='padding'>special offer</section>
+      <section className='bg-pale-blue padding'>customer review</section>
+      <section className='padding-x w-full sm:py-32 py-16'>subscribe</section>
+      <footer className='bg-black padding-x padding-t pb-8'>footer</footer>
+    </main>
+  )
 }
 export default App
